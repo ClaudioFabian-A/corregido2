deb9de246b025da044dbd937b60867e0ea6e3c68
ClaudioFabian-A <gonzalez.claudiofabian3@gmail.com>
2023-08-12 23:51:22 -0300

    5689

--- managers/cartsManager.js
+++ managers/cartsManager.js
@@ -1,4 +1,4 @@
-import { log } from "console";
+
 import fs from "fs";
 
 export default class cartsManager {
@@ -22,48 +22,56 @@ export default class cartsManager {
             }
         } catch (error) {
             console.log(error);
-        };
-        getCartById = async (id) => {
-            try {
+        }
+    };
+    getCartById = async (id) => {
+        try {
+            const { cid } = id;
+            if (fs.existsSync(this.path)) {
                 const cart = await this.getOllCart();
-                const eCart = cart.find((cart) => cart.id === id)
-                eCart ? eCart : "cart no encontrada"
-            } catch (error) {
-                console.log(error);
+                const eCart = cart.find((e) => e.id === parseInt(cid));
+                return eCart;
+            } else {
+                console.log("error el get");
             }
+        } catch (error) {
+            console.log(error);
         }
-        addCartWithId = async (cId, pId) => {
-            try {
-                const ollCarts = await this.getOllCart();
-                const ollCart = ollCarts.find((ollCart) => ollCart.id === cId)
-                const prodFind = ollCart.products.some((e) => e.pId === pId);
-                prodFind ? ollCart.products.find((e) = e.pId === pId).quantity++ : ollCart.products.push({ pId: pId, quantity: 1 });
-            } catch (error) {
-                console.log(error);
-
-            }
-        }
-        createId = async()=>{
-            try {
-                
-                
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        addCart = async () => {
+    }
+    addCartWithId = async (cId, pId) => {
+        try {
             const ollCarts = await this.getOllCart();
-            const id = await this.createId();
-            const nCart = { id, products: [] };
-            ollCarts.push(nCart);
-            await fs.promises.writeFile(this.path, JSON.stringify(ollCarts, null, '/t'));
+            const ollCart = ollCarts.find((ollCart) => ollCart.id === cId)
+            const prodFind = ollCart.products.some((e) => e.pId === pId);
+            prodFind ? ollCart.products.find((e) = e.pId === pId).quantity++ : ollCart.products.push({ pId: pId, quantity: 1 });
+        } catch (error) {
+            console.log(error);
 
         }
+    }
+    createId = async () => {
+        try {
+            if(fs.existsSync(this.path)){
+                const cIooCart= await fs.promises.readFile(this.path,"utf-8")
+            }
 
-
-
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    addCart = async () => {
+        const ollCarts = await this.getOllCart();
+        const id = await this.createId();
+        const nCart = { id, products: [] };
+        ollCarts.push(nCart);
+        await fs.promises.writeFile(this.path, JSON.stringify(ollCarts, null, '\t'));
 
     }
+
+
+
+
+
 
 
 
