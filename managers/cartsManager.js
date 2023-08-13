fb7ea50bc3177ee65e0af298f214a8747c5d11a0
ClaudioFabian-A <gonzalez.claudiofabian3@gmail.com>
2023-08-13 05:20:19 -0300

    456987

--- managers/cartsManager.js
+++ managers/cartsManager.js
@@ -1,7 +1,7 @@
 
 import fs from "fs";
 
-export default class cartsManager {
+export default class CartsManager {
     constructor(path) {
         this.path = path;
         this.carts = [];
@@ -9,12 +9,10 @@ export default class cartsManager {
     }
 
     getOllCart = async () => {
-
-
         try {
             if (fs.existsSync(this.path)) {
-                const data = await fs.promises.res.readFile(this.path, 'utf-8');
-                const cart = JSON.parse(data);
+                const data = await fs.promises.readFile(this.path, "utf-8")
+                const cart = JSON.parse(data)
                 return cart;
 
             } else {
@@ -26,28 +24,33 @@ export default class cartsManager {
     };
     getCartById = async (id) => {
         try {
-            const { cid } = id;
-            if (fs.existsSync(this.path)) {
-                const cart = await this.getOllCart();
-                const eCart = cart.find((e) => e.id === parseInt(cid));
+            // const { cid } = id;
+            // if (fs.existsSync(this.path)) {
+            const cart = await this.getOllCart();
+            const eCart = cart.find((e) => e.id === id);
+            if (eCart) {
+                console.log("ok");
                 return eCart;
             } else {
                 console.log("error el get");
             }
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    addCartWithId = async (cId, pId) => {
-        try {
-            const ollCarts = await this.getOllCart();
-            const ollCart = ollCarts.find((ollCart) => ollCart.id === cId)
-            const prodFind = ollCart.products.some((e) => e.pId === pId);
-            prodFind ? ollCart.products.find((e) = e.pId === pId).quantity++ : ollCart.products.push({ pId: pId, quantity: 1 });
+
         } catch (error) {
             console.log(error);
 
         }
+    }
+    addPCartWithId = async (cId, pId) => {
+        // try {
+        const ollCarts = await this.getOllCart();
+        const ollCart = ollCarts.find((e) => e.id === cId)
+        const prodFind = ollCart.products.findIndex((e) => e.pId === pId);
+        prodFind ? ollCart.products.find((e) = e.pId === pId).quantity++ : ollCart.products.push({ pId: pId, quantity: 1 });
+        await fs.promises.writeFile(this.path, JSON.stringify(ollCarts, null, 2))
+        // } catch (error) {
+        //     console.log(error);
+
+        // }
     }
     createId = async () => {
         try {
@@ -70,15 +73,5 @@ export default class cartsManager {
         await fs.promises.writeFile(this.path, JSON.stringify(ollCarts, null, 2));
 
     }
-
-
-
-
-
-
-
-
-
-
-
 }
+
--- routes/carts.router.js
+++ routes/carts.router.js
@@ -1,32 +1,32 @@
 import { Router } from "express";
 import CartsManager from "../managers/cartsManager.js";
 import { __dirname } from "../utils.js";
-const cartManager = new CartsManager(__dirname + "file/Carts.json");
+const manager = new CartsManager(__dirname + "/files/Carts.json");
 const router = Router();
 
 router.get('/carts', async (req, res) => {
-    const carts = await cartManager.getCarts();
-    carts.length === 0 ? res.status(200).json({ message: `No existen Carts` }) : res.status(400).json(carts);
+    const carts = await manager.getOllCart();
+    carts.length === 0 ? res.status(400).json({ message: `No existen Carts` }) : res.status(200).json(carts)
 
 })
 router.get('/carts/:cid', async (req, res) => {
     const cartsId = parseInt(req.params.cid);
-    const carts = await cartManager.getCartsById(cartsId);
+    const carts = await manager.getCartById(cartsId);
     carts ? res.status(200).json(carts) : res.status(400).json({ message: `Cart inexistente` })
 })
 router.post('/carts/', async (req, res) => {
-    const cart = await cartManager.addCart(req.body);
-    cart ? res.status(201).json({ message: `Cart creada con exito` }) : res.status(400).json({ message: `Error al crear el Cart`, cart })
+    const cart = await manager.addCart(req.body);
+    cart ? res.status(400).json({ message: `Error al crear el Cart`, cart }) : res.status(201).json({ message: `Cart creada con exito` })
 })
 router.post('/carts/:cid/products/:pid', async (req, res) => {
     try {
         const pId = parseInt(req.params.pid);
         const cId = parseInt(req.params.cid);
-        await cartManager.addCartWithId(pId, cId);
-        res.status(200).json({ message: "producto agregado con exito" })
+        await manager.addPCartWithId(pId, cId);
+        res.json({ message: "producto agregado con exito" })
     } catch (error) {
         console.log("Error al cargar el producto", error);
-        res.status(400).json({ message: `error al cargar el cart`, error })
+        res.status(400).json({ message: `error al cargar el cart` })
     }
 })
 router.delete('/carts/:cid', async (req, res) => {
@@ -36,7 +36,7 @@ router.delete('/carts/:cid', async (req, res) => {
 })
 router.put('/cart/:cid', async (req, res) => {
     const id = parseInt(req.params.id)
-    const cart = await cartManager.getCartsById(id);
+    const cart = await manager.getCartsById(id);
     cart ? res.status(200).json(cart) : res.status(400).json({ message: `Cart no agregada.` })
 })
 export default router
