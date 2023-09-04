427659353abb88e44d9e853737845f00fbe0e0c8
unknown <gonzalez.claudiofabian3@gmail.com>
2023-09-03 21:33:50 -0300

    5

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import viewRouter from './routes/views.router.js'
 import productRouter from "../src/routes/products.router.js"
 import cartRouter from "../src/routes/carts.router.js"
 import { Server } from 'socket.io'
-import { __dirname } from "./utils.js"
+import  __dirname  from './utils.js'
 const app = express();
 const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
@@ -14,7 +14,7 @@ const server = app.listen(PORT, () => {
 
 app.use(express.static(`${__dirname}/public`));
 app.engine('handlebars', Handlebars.engine());
-app.set('views', `${__dirname}`)
+app.set('views', `${__dirname}/views`)
 app.set('view engine', 'handlebars')
 
 app.use('/', viewRouter)
